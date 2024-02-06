103320c16c1b748329857bf35324daa820036dec
Jeremy <jeremy.gaudin@ynov.com>
2024-02-06 12:34:14 +0100

    added way to collect data from forms in views.py

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint(__name__, "views")
 
@@ -16,4 +16,19 @@ def page():
     args =request.args
     name = args.get('name') #/name=
     return render_template("index.html", name=name)
+
+@views.route("/sendinfo" , methodes = ['GET','POST'])
+def info():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        lastname = request.form.get('lastname')
+
+        if len(name) <= 0:
+            flash('Please input name', category='error')
+        elif len(lastname) <= 0:
+            flash('Please input last name', category='error')
+        else:
+            flash('yay name and lastname are valid!', category='success')
+            #add to database
+    return render_template("index.html")
 
