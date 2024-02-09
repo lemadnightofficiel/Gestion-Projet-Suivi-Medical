c675fba63469335c61c7c7f344e1d9bb60edd856
cmatthie <matthieu.caron@ynov.com>
2024-02-09 09:50:27 +0100

    Add delete_allgraph function

--- graph_functions.py
+++ graph_functions.py
@@ -5,9 +5,9 @@ import os
 def bpm_graph(data_list): #To display a BPM graph
     
     time = datetime.now().strftime("%Y-%m-%d") #Set the time
-
-    if os.path.isfile(f"static/images/bpm_graph_{time}.png"): #Check if the existing file 
-        os.remove(f"static/images/bpm_graph_{time}.png") #Delete to replace the file with an other image
+    
+    if os.path.isfile(f"static/graph/bpm_graph_{time}.png"): #Check if the existing file 
+        os.remove(f"static/graph/bpm_graph_{time}.png") #Delete to replace the file with an other image
     
     bpm_fig = plt.figure("BPM") #Create a figure for avoid the elements accumulation in the graph
 
@@ -22,15 +22,15 @@ def bpm_graph(data_list): #To display a BPM graph
     plt.ylabel("BPM")
     plt.title('Suivi de votre BPM :')
 
-    plt.savefig(f"static/images/bpm_graph_{time}.png") #Save the file with the date
-    return "static/images/bpm_graph_{time}.png"
+    plt.savefig(f"static/graph/bpm_graph_{time}.png") #Save the file with the date
+    return "static/graph/bpm_graph_{time}.png"
 
 def oxysat_graph(data_list): #To display an oxygen saturation graph
 
     time = datetime.now().strftime("%Y-%m-%d") #Set the time
 
-    if os.path.isfile(f"static/images/oxysat_graph_{time}.png"):  #Check if the existing file
-        os.remove(f"static/images/oxysat_graph_{time}.png") #Delete to replace the file with an other image
+    if os.path.isfile(f"static/graph/oxysat_graph_{time}.png"):  #Check if the existing file
+        os.remove(f"static/graph/oxysat_graph_{time}.png") #Delete to replace the file with an other image
     
     oxysat_fig = plt.figure("OxySat")  #Create a figure for avoid the elements accumulation in the graph
 
@@ -46,15 +46,15 @@ def oxysat_graph(data_list): #To display an oxygen saturation graph
     plt.ylabel("Saturation oxygène")
     plt.title('Suivi de votre saturation en oxygène :')
 
-    plt.savefig(f"static/images/oxysat_graph_{time}.png") #Save the file with the date
-    return "static/images/oxysat_graph_{time}.png"
+    plt.savefig(f"static/graph/oxysat_graph_{time}.png") #Save the file with the date
+    return "static/graph/oxysat_graph_{time}.png"
 
 def imc_graph(data_list): #To display an imc graph
 
     time = datetime.now().strftime("%d-%m-%Y") #Set the time
 
-    if os.path.isfile(f"static/images/imc_graph_{time}.png"): #Check if the existing file
-        os.remove(f"static/images/imc_graph_{time}.png") #Delete to replace the file with an other image
+    if os.path.isfile(f"static/graph/imc_graph_{time}.png"): #Check if the existing file
+        os.remove(f"static/graph/imc_graph_{time}.png") #Delete to replace the file with an other image
 
     imc_fig = plt.figure("IMC") #Create a figure for avoid the elements accumulation in the graph
 
@@ -70,15 +70,15 @@ def imc_graph(data_list): #To display an imc graph
     plt.ylabel("IMC")
     plt.title('Suivi de votre IMC :')
 
-    plt.savefig(f"static/images/imc_graph_{time}.png") #Save the file with the date
-    return "static/images/imc_graph_{time}.png"
+    plt.savefig(f"static/graph/imc_graph_{time}.png") #Save the file with the date
+    return "static/graph/imc_graph_{time}.png"
 
 def pressure_graph(data_list): #To display a pressure graph
 
     time = datetime.now().strftime("%Y-%m-%d") #Set the time
     
-    if os.path.isfile(f"static/images/pressure_graph_{time}.png"): #Check if the existing file
-        os.remove(f"static/images/pressure_graph_{time}.png") #Delete to replace the file with an other image
+    if os.path.isfile(f"static/graph/pressure_graph_{time}.png"): #Check if the existing file
+        os.remove(f"static/graph/pressure_graph_{time}.png") #Delete to replace the file with an other image
 
     pressure_fig = plt.figure("Pressure") #Create a figure for avoid the elements accumulation in the graph
 
@@ -97,5 +97,10 @@ def pressure_graph(data_list): #To display a pressure graph
     plt.ylabel("Pressions cardiovasculaires")
     plt.title('Suivi de votre Tension :')
 
-    plt.savefig(f"static/images/pressure_graph_{time}.png") #Save the file with the date
-    return "static/images/pressure_graph_{time}.png"
+    plt.savefig(f"static/graph/pressure_graph_{time}.png") #Save the file with the date
+    return "static/graph/pressure_graph_{time}.png"
+
+def delete_allgraph():
+    for graph in os.listdir('static/graph'):
+        if os.path.isfile(os.path.join('static/graph', graph)):
+            os.remove(os.path.join('static/graph', graph))
